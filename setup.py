d77877a86bafa31456afa34b004c8a3c6a6de613
KeFiK__ <152422985+KeFiKkK@users.noreply.github.com>
2025-03-20 15:27:51 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ke_api_minecraft",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=["requests"],
+    description="Библиотека для работы с API Minecraft",
+    author="KeFiKkK",
+    author_email="chistjdlacsgo@gmail.com",
+    url="https://github.com/KeFiKkK/ke_api_minecraft",
+)
